08daebc21afc6a0d109c6889167a71c8f8d539c8
ronaldo0820 <95460329+ronaldo0820@users.noreply.github.com>
2022-03-02 16:01:03 +0530

    Added a Java Program

---
+++ Main.java
@@ -0,0 +1,13 @@
+package com.ronaldo;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner input = new Scanner(System.in);
+        int number1 = 20;
+        System.out.println(number1);
+    }
+}
