dfa7dfc18cb8835c9ee471809c2974e1b3ffb8ea
RONALDO MATTHEW J R <95460329+ronaldo0820@users.noreply.github.com>
2022-03-18 09:36:15 +0530

    Create Convert char to String.java

---
+++ Convert char to String.java
@@ -0,0 +1,9 @@
+
+public class Main
+{
+	public static void main(String[] args) {
+        char ch = 'c';
+        String st = Character.toString(ch);
+        System.out.println("The string is: " + st);
+    }
+}
